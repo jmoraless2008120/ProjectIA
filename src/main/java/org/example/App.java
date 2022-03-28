64dd689e384dd697041a0c4f8ee0c03ac8b9237d
User <2008120@newton.pe>
2022-03-28 09:50:51 -0500

    FirstUpload

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 public class App extends Application {
 
     private static Scene scene;
-
+//
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("MainScreen"));
